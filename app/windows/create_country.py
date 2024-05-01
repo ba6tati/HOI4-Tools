54de67a65ef47352c8b57685852dd03a04535c5d
Ba6taTi <krasizh07@gmail.com>
2024-05-01 17:09:50 +0300

    Visual Improvement CreateCountryWindow

--- app/windows/create_country.py
+++ app/windows/create_country.py
@@ -12,31 +12,39 @@ class CreateCountryWindow(CTkToplevel):
         self.title(WINDOW_TITLE)
         self.geometry(WINDOW_SIZE)
         
-        self.mod_path = CTkEntry(self, placeholder_text='Mod Path')
+        self.frame = CTkFrame(self)
+        
+        
+        self.mod_path = CTkEntry(self.frame, placeholder_text='Mod Path')
         
         if mod_path:
             self.mod_path.insert(0, mod_path)
         
-        self.select_path_btn = CTkButton(self, text='Select', command=self.select_directory)
+        self.select_path_btn = CTkButton(self.frame, text='Select', command=self.select_directory)
         
-        self.tag = CTkEntry(self, placeholder_text='TAG')
-        self.name = CTkEntry(self, placeholder_text='Name')
-        self.culture = CTkOptionMenu(self, values=cultures)
+        self.tag = CTkEntry(self.frame, placeholder_text='TAG')
+        self.name = CTkEntry(self.frame, placeholder_text='Name')
+        self.culture = CTkOptionMenu(self.frame, values=cultures)
         
-        self.color = CTkEntry(self, placeholder_text='Color')
-        self.select_color_btn = CTkButton(self, text='Select Color', command=self.select_color)
+        self.color = CTkEntry(self.frame, placeholder_text='R G B')
+        self.select_color_btn = CTkButton(self.frame, text='Select Color', command=self.select_color)
         
-        self.generate_btn = CTkButton(self, text='Generate', command=self.generate)
+        self.generate_btn = CTkButton(self.frame, text='Generate', command=self.generate)
         
-        self.mod_path.grid(row=0, column=0, padx=10, pady=10)
-        self.select_path_btn.grid(row=0, column=1, padx=10, pady=10)
+        self.mod_path.grid(row=0, column=0)
+        self.select_path_btn.grid(row=0, column=1)
         
         self.tag.grid(row=1, column=0, columnspan=2)
         self.name.grid(row=2, column=0, columnspan=2)
         self.culture.grid(row=3, column=0, columnspan=2)
-        self.color.grid(row=4, column=0, columnspan=2)
-        self.select_color_btn.grid(row=5, column=0, columnspan=2)
-        self.generate_btn.grid(row=6, column=0, columnspan=2)
+        self.color.grid(row=4, column=0)
+        self.select_color_btn.grid(row=4, column=1)
+        self.generate_btn.grid(row=5, column=0, columnspan=2)
+            
+        self.frame.place(relx=0.5, rely=0.5, anchor=CENTER)
+        
+        for widget in self.frame.winfo_children():
+            widget.grid_configure(padx=10, pady=10, sticky=NSEW)
         
     def select_color(self):
         color = AskColor()
@@ -44,6 +52,8 @@ class CreateCountryWindow(CTkToplevel):
         
         self.color.delete(0, END)
         self.color.insert(0, color)
+        
+        self.after(10, self.lift)
         
     def select_directory(self):
         mod_path = filedialog.askdirectory()
